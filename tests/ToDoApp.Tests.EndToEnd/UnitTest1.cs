5c756b977399ca191aef5861ed18f545e5e9ef53
Ion Gritco <63732333+iongritco-sv@users.noreply.github.com>
2022-12-08 12:51:57 +0200

    add default solution files

---
+++ tests/ToDoApp.Tests.EndToEnd/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ToDoApp.Tests.EndToEnd
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
