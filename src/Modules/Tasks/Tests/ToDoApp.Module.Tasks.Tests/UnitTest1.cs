de4a5d3af52e78dd90dd2cceef2c9caa53bf1f20
Ion Gritco <63732333+iongritco-sv@users.noreply.github.com>
2022-12-09 16:49:36 +0200

    add test projects

---
+++ src/Modules/Tasks/Tests/ToDoApp.Module.Tasks.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToDoApp.Module.Tasks.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Modules/Users/Tests/ToDoApp.Modules.Users.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToDoApp.Modules.Users.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
