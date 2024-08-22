70f6ccdb3d2b020d25da8b1b63c30c0748b9cb91
Ion Gritco <ion.gritco.work@gmail.com>
2024-08-22 14:23:25 +0300

    add architecture tests

--- tests/ToDoApp.Architecture.Tests/ArchitectureSetup.cs
+++ tests/ToDoApp.Architecture.Tests/ArchitectureSetup.cs
@@ -1,7 +1,17 @@
 using System.Reflection;
+using ToDoApp.Modules.Emails.API;
+using ToDoApp.Modules.Emails.Application.Interfaces;
+using ToDoApp.Modules.Emails.Domain.Entities;
+using ToDoApp.Modules.Emails.Infrastructure;
+using ToDoApp.Modules.Tasks.API;
+using ToDoApp.Modules.Tasks.Application.Interfaces;
+using ToDoApp.Modules.Tasks.Domain.Entities;
+using ToDoApp.Modules.Tasks.Infrastructure;
+using ToDoApp.Modules.Tasks.Persistence.ToDo;
 using ToDoApp.Modules.Users.API;
 using ToDoApp.Modules.Users.Application.Interfaces;
 using ToDoApp.Modules.Users.Domain.Entities;
+using ToDoApp.Modules.Users.Identity.Users;
 using ToDoApp.Modules.Users.Persistence;
 
 namespace ToDoApp.Architecture.Tests;
@@ -11,12 +21,34 @@ public class ArchitectureSetup
 	// Users module
 	protected static readonly Assembly UsersDomainAssembly = typeof(User).Assembly;
 	protected static readonly Assembly UsersApplicationAssembly = typeof(IIdentityService).Assembly;
-	protected static readonly Assembly UsersIdentityAssembly = typeof(IIdentityService).Assembly;
+	protected static readonly Assembly UsersIdentityAssembly = typeof(IdentityService).Assembly;
 	protected static readonly Assembly UsersPersistenceAssembly = typeof(UsersContext).Assembly;
 	protected static readonly Assembly UsersPresentationAssembly = typeof(UsersModule).Assembly;
 	protected static readonly string? UsersDomainAssemblyName = UsersDomainAssembly.GetName().Name;
 	protected static readonly string? UsersApplicationAssemblyName = UsersApplicationAssembly.GetName().Name;
 	protected static readonly string? UsersIdentityAssemblyName = UsersIdentityAssembly.GetName().Name;
 	protected static readonly string? UsersPersistenceAssemblyName = UsersPersistenceAssembly.GetName().Name;
-	protected static readonly string? UsersPresentationAssemblyName = UsersPresentationAssembly.GetName().Name; 
+	protected static readonly string? UsersPresentationAssemblyName = UsersPresentationAssembly.GetName().Name;
+	
+	// Tasks module
+	protected static readonly Assembly TasksDomainAssembly = typeof(ToDoItem).Assembly;
+	protected static readonly Assembly TasksApplicationAssembly = typeof(ITasksCommandRepository).Assembly;
+	protected static readonly Assembly TasksInfrastructureAssembly = typeof(UsersApiClient).Assembly;
+	protected static readonly Assembly TasksPersistenceAssembly = typeof(TasksCommandRepository).Assembly;
+	protected static readonly Assembly TasksPresentationAssembly = typeof(TasksModule).Assembly;
+	protected static readonly string? TasksDomainAssemblyName = TasksDomainAssembly.GetName().Name;
+	protected static readonly string? TasksApplicationAssemblyName = TasksApplicationAssembly.GetName().Name;
+	protected static readonly string? TasksInfrastructureAssemblyName = TasksInfrastructureAssembly.GetName().Name;
+	protected static readonly string? TasksPersistenceAssemblyName = TasksPersistenceAssembly.GetName().Name;
+	protected static readonly string? TasksPresentationAssemblyName = TasksPresentationAssembly.GetName().Name;
+	
+	// Emails module
+	protected static readonly Assembly EmailsDomainAssembly = typeof(Email).Assembly;
+	protected static readonly Assembly EmailsApplicationAssembly = typeof(IEmailService).Assembly;
+	protected static readonly Assembly EmailsInfrastructureAssembly = typeof(EmailService).Assembly;
+	protected static readonly Assembly EmailsPresentationAssembly = typeof(EmailsModule).Assembly;
+	protected static readonly string? EmailsDomainAssemblyName = EmailsDomainAssembly.GetName().Name;
+	protected static readonly string? EmailsApplicationAssemblyName = EmailsApplicationAssembly.GetName().Name;
+	protected static readonly string? EmailsInfrastructureAssemblyName = EmailsInfrastructureAssembly.GetName().Name;
+	protected static readonly string? EmailsPresentationAssemblyName = EmailsPresentationAssembly.GetName().Name; 
 }
--- tests/ToDoApp.Architecture.Tests/ArchitectureTests.cs
+++ tests/ToDoApp.Architecture.Tests/ArchitectureTests.cs
@@ -1,4 +1,144 @@
-﻿namespace ToDoApp.Architecture.Tests;
-public class ArchitectureTests
+﻿using NetArchTest.Rules;
+using System.Reflection;
+using FluentAssertions;
+
+namespace ToDoApp.Architecture.Tests;
+public class ArchitectureTests : ArchitectureSetup
 {
+	[Fact]
+	public void DomainLayer_ShouldNotDependOn_AnyOtherLayer()
+	{
+		// Assert
+		string[] dependencies =
+		[
+			UsersApplicationAssemblyName, UsersIdentityAssemblyName, UsersPersistenceAssemblyName, UsersPresentationAssemblyName,
+			TasksApplicationAssemblyName, TasksInfrastructureAssemblyName, TasksPersistenceAssemblyName, TasksPresentationAssemblyName,
+			EmailsApplicationAssemblyName, EmailsInfrastructureAssemblyName, EmailsPresentationAssemblyName
+		];
+
+		Assembly[] assemblies = [UsersDomainAssembly, TasksDomainAssembly, EmailsDomainAssembly];
+
+		// Act
+		var isSuccessful = Types.InAssemblies(assemblies)
+			.Should()
+			.NotHaveDependencyOnAny(dependencies)
+			.GetResult().IsSuccessful;
+
+		// Assert
+		isSuccessful.Should().BeTrue("Domain layers should not have any dependency on other projects");
+	}
+
+	[Fact]
+	public void ApplicationLayer_ShouldDependOnlyOn_DomainLayer()
+	{
+		// Assert
+		string[] dependencies =
+		[
+			UsersIdentityAssemblyName, UsersPersistenceAssemblyName, UsersPresentationAssemblyName,
+			TasksInfrastructureAssemblyName, TasksPersistenceAssemblyName, TasksPresentationAssemblyName,
+			EmailsInfrastructureAssemblyName, EmailsPresentationAssemblyName
+		];
+
+		Assembly[] assemblies = [UsersApplicationAssembly, TasksApplicationAssembly, EmailsApplicationAssembly];
+
+		// Act
+		var isSuccessful = Types.InAssemblies(assemblies)
+			.Should()
+			.NotHaveDependencyOnAny(dependencies)
+			.GetResult().IsSuccessful;
+
+		// Assert
+		isSuccessful.Should().BeTrue("Application layers should depend only on Domain layers");
+	}
+
+	[Fact]
+	public void InfrastructureLayer_ShouldNotDependOn_PresentationOrPersistenceLayer()
+	{
+		// Assert
+		string[] dependencies =
+		[
+			UsersPersistenceAssemblyName, UsersPresentationAssemblyName,
+			TasksPersistenceAssemblyName, TasksPresentationAssemblyName,
+			EmailsPresentationAssemblyName
+		];
+
+		Assembly[] assemblies = [UsersIdentityAssembly, TasksInfrastructureAssembly, EmailsInfrastructureAssembly];
+
+		// Act
+		var isSuccessful = Types.InAssemblies(assemblies)
+			.Should()
+			.NotHaveDependencyOnAny(dependencies)
+			.GetResult().IsSuccessful;
+
+		// Assert
+		isSuccessful.Should().BeTrue("Infrastructure layers should not depend on Presentation or Persistence layers");
+	}
+
+	[Fact]
+	public void UsersModule_ShouldNotDependOn_AnyOtherModule()
+	{
+		// Assert
+		string[] dependencies =
+		[
+			TasksDomainAssemblyName, TasksApplicationAssemblyName, TasksInfrastructureAssemblyName, TasksPersistenceAssemblyName, TasksPresentationAssemblyName,
+			EmailsDomainAssemblyName, EmailsApplicationAssemblyName, EmailsInfrastructureAssemblyName, EmailsPresentationAssemblyName
+		];
+
+		Assembly[] assemblies = [UsersDomainAssembly, UsersApplicationAssembly, UsersIdentityAssembly, UsersPersistenceAssembly, UsersPresentationAssembly];
+
+		// Act
+		var isSuccessful = Types.InAssemblies(assemblies)
+			.Should()
+			.NotHaveDependencyOnAny(dependencies)
+			.GetResult().IsSuccessful;
+
+		// Assert
+		isSuccessful.Should().BeTrue("Users module should not have any direct dependencies to other modules");
+	}
+
+	[Fact]
+	public void TasksModule_ShouldNotDependOn_AnyOtherModule()
+	{
+		// Assert
+		string[] dependencies =
+		[
+			UsersDomainAssemblyName, UsersApplicationAssemblyName, UsersIdentityAssemblyName, UsersPersistenceAssemblyName, UsersPresentationAssemblyName,
+			EmailsDomainAssemblyName, EmailsApplicationAssemblyName, EmailsInfrastructureAssemblyName, EmailsPresentationAssemblyName
+		];
+
+		Assembly[] assemblies = [TasksDomainAssembly, TasksApplicationAssembly, TasksInfrastructureAssembly, TasksPersistenceAssembly, TasksPresentationAssembly];
+
+		// Act
+		var result = Types.InAssemblies(assemblies)
+			.Should()
+			.NotHaveDependencyOnAny(dependencies)
+			.GetResult();
+			
+		var isSuccessful=result.IsSuccessful;
+
+		// Assert
+		isSuccessful.Should().BeTrue("Tasks module should not have any direct dependencies to other modules");
+	}
+
+	[Fact]
+	public void EmailsModule_ShouldNotDependOn_AnyOtherModule()
+	{
+		// Assert
+		string[] dependencies =
+		[
+			UsersDomainAssemblyName, UsersApplicationAssemblyName, UsersIdentityAssemblyName, UsersPersistenceAssemblyName, UsersPresentationAssemblyName,
+			TasksDomainAssemblyName, TasksApplicationAssemblyName, TasksInfrastructureAssemblyName, TasksPersistenceAssemblyName, TasksPresentationAssemblyName
+		];
+
+		Assembly[] assemblies = [EmailsDomainAssembly, EmailsApplicationAssembly, EmailsInfrastructureAssembly, EmailsPresentationAssembly];
+
+		// Act
+		var isSuccessful = Types.InAssemblies(assemblies)
+			.Should()
+			.NotHaveDependencyOnAny(dependencies)
+			.GetResult().IsSuccessful;
+
+		// Assert
+		isSuccessful.Should().BeTrue("Emails module should not have any direct dependencies to other modules");
+	}
 }
