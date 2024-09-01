3e1a7d76d3e887cb7c5d804c173ac4249e6d67fd
iongritco <ion.gritco@gmail.com>
2024-09-01 19:10:07 +0300

    add health check endpoint and UI

--- README.md
+++ README.md
@@ -7,6 +7,19 @@ A playground for experimenting projects with a Modular Monolith Architecture - D
 - To get started, just create the databases ToDo and ToDo_Tests and update the connection string in appsettings (both in ToDoApp.Server.API and in ToDoApp.Tests.EndToEnd for integration tests) - the migration will be executed automatically on the first run. 
 - Set as startup the ToDoApp.Client.Blazor and ToDoApp.Server.API projects.
 
+### Health checks
+Server has both API and UI for checking the health of dependencies, in this case SQL Server and MassTransit:
+- API - default address: https://localhost:5002/api/health
+- UI - default address: https://localhost:5002/healthchecks-ui
+
+### Modules communications
+There are two ways of communicating between modules:
+1) Asynchronous - via MassTransit that runs locally
+2) Synchronous - in two ways, you can check TasksModule for reference, CreateToDoCommandHandler command:
+    - via Contracts projects - direct dependency on Contracts project without direct dependency on any other module component
+    - via SyncBus - dynamically by providing a route that runs dynamically the request, no direct dependency
+    
+
 
 ## Credits
 - This implementation is based on the original implementation from https://github.com/devmentors/ModularMonolith and https://github.com/kgrzybek/modular-monolith-with-ddd
--- src/Bootstrapper/ToDoApp.Server.API/Program.cs
+++ src/Bootstrapper/ToDoApp.Server.API/Program.cs
@@ -1,8 +1,18 @@
 using System.Reflection;
+
 using FluentValidation;
+
+using HealthChecks.UI.Client;
+
 using MediatR;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+
 using ToDoApp.Common;
 using ToDoApp.EventBus.MassTransit;
 using ToDoApp.Modules.Emails.API;
@@ -21,6 +31,19 @@ namespace ToDoApp.Server.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add health checks
+            builder.Services
+                .AddHealthChecks()
+                .AddSqlServer(builder.Configuration["ConnectionStrings:ToDoConnection"], healthQuery: "select 1", name: "SQL Server", failureStatus: HealthStatus.Unhealthy, tags: new[] { "Database" });
+            builder.Services.AddHealthChecksUI(opt =>
+            {
+                opt.SetEvaluationTimeInSeconds(10); //time in seconds between check    
+                opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks    
+                opt.SetApiMaxActiveRequests(1); //api requests concurrency    
+                opt.AddHealthCheckEndpoint("TODO API", "/api/health"); //map health check api    
+
+            }).AddInMemoryStorage();
 
             // Add modules
             builder.Services.AddTasksModule(builder.Configuration);
@@ -84,6 +107,13 @@ namespace ToDoApp.Server.API
 
             app.UseAuthorization();
             app.MapControllers();
+
+            app.MapHealthChecks("/api/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            app.MapHealthChecksUI();
 
             app.Run();
         }
